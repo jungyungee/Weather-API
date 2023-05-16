4b9566830ab1b7cebdccd6215660ba1a08f4f35b
jungyungee <86039030+jungyungee@users.noreply.github.com>
2023-05-16 19:27:01 +0900

    날씨 API 사용 예제

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
 
 function App() {
+  const [location, setLocation] = useState('');
+  const [result, setResult] = useState({});
+  const API_KEY ="55c62c295e95adbb3276e4f8b6b27c81";
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
+
+  const searchWeather = async (e) => {
+    if (e.key === 'Enter'){
+      try{
+        const data = await axios({
+          method: 'get',
+          url: url
+        })
+        console.log(data);
+        setResult(data);
+      }
+      catch (err) {
+        alert(err);
+      }
+    }
+  } //비동기 방식이 api 사용
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrap>
+      <div className='appContentWrap'>
+        <input  
+          placeholder='도시를 입력하세요' value={location} 
+          onChange={(e)=>setLocation(e.target.value)}
+          type='text'
+          onKeyDown={searchWeather}
+          />
+          {
+            Object.keys(result).length !==0 && (
+              <ResultWrap>
+                <div className='city'>{result.data.name}</div>
+                <div className='temp'>{result.data.main.temp}</div>
+                <div className='sky'>{result.data.weather[0].main}</div>
+              </ResultWrap>
+            
+            )}
+      </div>
+    </AppWrap>
   );
 }
 
 export default App;
+
+const AppWrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  border: 1px red solid;
+
+  .appContentWrap{
+    left: 50%;
+    top: 50%;
+    transfrom: translate(-50%, -50%);
+    position: absolute;
+    border: 1px blue solid;
+    padding: 20px;
+  }
+`;
+
+const ResultWrap = styled.div`
+  margin-top: 60px;
+  padding: 10px;
+  border: 1px black solid;
+  border-radius: 8px;
+  .city{
+    font-size: 24px;
+  }
+  .temparature{
+    font-size: 60px;
+    margin-top: 8px;
+  }
+  .sky{
+    font-size: 20px;
+    text-align: right;
+    margin-top: 8px;
+  }
+`;
+
+//유엠씨 실습- 날씨 앱
